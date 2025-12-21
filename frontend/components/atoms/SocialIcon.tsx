55874b684cdf3161b80da315fd3b17c55aa48957
Carlos Daniel <144691269+KarlosNiel@users.noreply.github.com>
2025-12-21 20:59:59 -0300

    feat(frontend): feat(frontend): ✨ add social media icons constant and � start using animated icons from lucide-animated - � installed shadcn - ✨ began using animated icons from lucide-animated for more attractive design - � adjusted atomic SocialIcon component

--- frontend/components/atoms/SocialIcon.tsx
+++ frontend/components/atoms/SocialIcon.tsx
@@ -1,19 +1,22 @@
+import Link from "next/link"
+
 type SocialIconProps = {
     href: string
-    icon: React.ReactNode
+    icon: React.ReactNode | string
     label: string
 }
 
 export function SocialIcon({ href, icon, label}: SocialIconProps){
     return (
-        <a 
+        <Link 
             href={href}
             target="_blank"
             rel="noopener noreferrer"
             aria-label={label}
-            className=""
+            title={label}
+            className="size-5"
         >
             {icon}
-        </a>
+        </Link>
     )
 }
